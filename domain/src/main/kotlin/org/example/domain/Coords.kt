95e1d45d95443413e927663ca82518e978b24316
Krystian Zielonka <krystian.zielonka@tomtom.com>
2026-07-12 13:42:00 +0200

    improve performance by properly packing bits for vector2D

--- domain/src/main/kotlin/org/example/domain/Coords.kt
+++ domain/src/main/kotlin/org/example/domain/Coords.kt
@@ -1,43 +1,5 @@
 package org.example.domain
 
-@JvmInline
-value class ChunkLocalVector2D(private val v: Vector2D) {
-    constructor(x: Int, y: Int) : this(Vector2D(x, y))
-    val x: Int
-        get() = v.x
-
-    val y: Int
-        get() = v.y
-
-    override fun toString(): String {
-        return "ChunkLocalVector2D(y=$y, x=$x)"
-    }
-}
-
-@JvmInline
-value class ChunkIndexVector2D(private val v: Vector2D) {
-    constructor(x: Int, y: Int) : this(Vector2D(x, y))
-    val x: Int
-        get() = v.x
-
-    val y: Int
-        get() = v.y
-
-    override fun toString(): String {
-        return "ChunkIndexVector2D(y=$y, x=$x)"
-    }
-}
-
-@JvmInline
-value class GlobalVector2D(private val v: Vector2D) {
-    constructor(x: Int, y: Int) : this(Vector2D(x, y))
-    val x: Int
-        get() = v.x
-
-    val y: Int
-        get() = v.y
-
-    override fun toString(): String {
-        return "GlobalVector2D(y=$y, x=$x)"
-    }
-}
+typealias ChunkLocalVector2D = Vector2D
+typealias ChunkIndexVector2D = Vector2D
+typealias GlobalVector2D = Vector2D
--- domain/src/main/kotlin/org/example/domain/Vector2D.kt
+++ domain/src/main/kotlin/org/example/domain/Vector2D.kt
@@ -3,14 +3,14 @@ package org.example.domain
 @JvmInline
 value class Vector2D(private val l: Long) {
     constructor(x: Int, y: Int) : this(
-        (x.toLong() shl 32) or (y.toLong() and 0xffffffffL)
+        (x.toLong() shl 32) or ((y + x * 31).toLong() and 0xffffffffL)
     )
 
     val x: Int
         get() = (l shr 32).toInt()
 
     val y: Int
-        get() = l.toInt()
+        get() = l.toInt() - x * 31
 
     override fun toString(): String {
         return "Vector2D(y=$y, x=$x)"
